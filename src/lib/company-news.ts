781945374fbff5b129816c22fac34d49577ff5ed
gljc1117 <liyingxi49@gmail.com>
2026-09-15 16:38:56 +0800

    Publish authorized Xiaogan 3D guide assisted lung biopsy case

--- src/lib/company-news.ts
+++ src/lib/company-news.ts
@@ -1,3 +1,4 @@
+import { XIAOGAN_LUNG_BIOPSY } from "./xiaogan-lung-biopsy";
 import { RSI_COMMENTARY } from "./rsi-commentary";
 import { FLY_CONNECTOME_REVIEW } from "./fly-connectome-review";
 import { AI_WORKFORCE_COMMENTARY } from "./ai-workforce-commentary";
@@ -19,6 +20,7 @@ export type CompanyNewsArticle = Omit<EditorialArticle, "caseProfile"> & {
 };
 
 export const COMPANY_NEWS_ARTICLES: CompanyNewsArticle[] = [
+XIAOGAN_LUNG_BIOPSY,
 RSI_COMMENTARY,
 FLY_CONNECTOME_REVIEW,
 AI_WORKFORCE_COMMENTARY,
