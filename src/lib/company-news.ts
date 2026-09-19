323c5bdaec1b6c0cb5b7ed35ad3d1f04157c1a73
gljc1117 <liyingxi49@gmail.com>
2026-09-19 11:13:49 +0800

    Publish responsible clinical AI commentary with cited governance framework

--- src/lib/company-news.ts
+++ src/lib/company-news.ts
@@ -1,3 +1,4 @@
+import { RESPONSIBLE_CLINICAL_AI } from "./responsible-clinical-ai";
 import { MEDICAL_AI_REGULATION } from "./medical-ai-regulation";
 import { GRANT_DECISION_FRAMEWORK } from "./grant-decision-framework";
 import { RECURSIVE_INSTRUMENTS_REVIEW } from "./recursive-instruments-review";
@@ -23,6 +24,7 @@ export type CompanyNewsArticle = Omit<EditorialArticle, "caseProfile"> & {
 };
 
 export const COMPANY_NEWS_ARTICLES: CompanyNewsArticle[] = [
+RESPONSIBLE_CLINICAL_AI,
 MEDICAL_AI_REGULATION,
 GRANT_DECISION_FRAMEWORK,
 RECURSIVE_INSTRUMENTS_REVIEW,
